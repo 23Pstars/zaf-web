74fd673b94f6de490ec4fd77e8eae43d93bebd7c
Ahmad Zafrullah <23Pstars@gmail.com>
2019-09-14 13:55:43 +0800

    init v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,35 @@
-const fs = require('fs');
 const gulp = require('gulp');
-const mustache = require('gulp-mustache');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
 const htmlclean = require('gulp-htmlclean');
-
-let config = JSON.parse(fs.readFileSync('./config.json'));
+const cssnano = require('gulp-cssnano');
 
 gulp.task('html', done => {
-    config.build.path = config.build.dev.path;
-    gulp.src(config.build.dev.src + '/*.html')
-        .pipe(mustache(config))
+    gulp.src('./index.html')
         .pipe(htmlclean())
-        .pipe(gulp.dest(config.build.dev.dst));
-    done();
-});
-
-gulp.task('html-local', done => {
-    config.build.path = config.build.local.path;
-    gulp.src(config.build.dev.src + '/*.html')
-        .pipe(mustache(config))
-        .pipe(htmlclean())
-        .pipe(gulp.dest(config.build.local.dst));
+        .pipe(gulp.dest('_build'));
     done();
 });
 
 gulp.task('css', done => {
-    gulp.src(config.build.dev.src + '/assets/scss/*.scss')
-        .pipe(sass({
-            outputStyle: "compressed"
-        }))
-        .pipe(gulp.dest(config.build.dev.dst + '/assets/css'));
-    done();
-});
-
-gulp.task('js', done => {
-    gulp.src(config.build.dev.src + '/assets/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest(config.build.dev.dst + '/assets/js'));
+    gulp.src(['./assets/css/ie8.css', './assets/css/ie9.css', './assets/css/main.css', './assets/css/noscript.css'])
+        .pipe(cssnano())
+        .pipe(gulp.dest('_build/assets/css'));
     done();
 });
 
 gulp.task('copy', done => {
-    gulp.src(['node_modules/foundation-sites/css/foundation.min.css',
-            'node_modules/normalize-css/normalize.css'
-        ])
-        .pipe(gulp.dest(config.build.dev.dst + '/assets/css'));
-    gulp.src(['node_modules/foundation-icons/foundation-icons*'])
-        .pipe(gulp.dest(config.build.dev.dst + '/assets/font'));
-    gulp.src([config.build.dev.src + '/assets/img/*'])
-        .pipe(gulp.dest(config.build.dev.dst + '/assets/img'));
-    gulp.src([config.build.dev.src + '/*.txt'])
-        .pipe(gulp.dest(config.build.dev.dst));
-    gulp.src([config.build.dev.src + '/.htaccess'])
-        .pipe(gulp.dest(config.build.dev.dst));
+    gulp.src('./assets/js/*.js')
+        .pipe(gulp.dest('_build/assets/js'));
+    gulp.src('./assets/css/font-awesome.min.css')
+        .pipe(gulp.dest('_build/assets/css'));
+    gulp.src('./assets/css/images/*')
+        .pipe(gulp.dest('_build/assets/css/images'));
+    gulp.src('./assets/fonts/*')
+        .pipe(gulp.dest('_build/assets/fonts'));
+    gulp.src('./images/*')
+        .pipe(gulp.dest('_build/images'));
+    gulp.src('./.htaccess')
+        .pipe(gulp.dest('_build'));
     done();
 });
 
-gulp.task('default', gulp.parallel(['copy', 'html', 'css', 'js']));
-gulp.task('local', gulp.parallel(['copy', 'html-local', 'css', 'js']));
-
-gulp.task('watch', gulp.parallel(['default'], () => {
-    gulp.watch(config.build.dev.src + '/*.html', ['html']);
-    gulp.watch(config.build.dev.src + '/assets/scss/*.scss', ['css']);
-    gulp.watch(config.build.dev.src + '/assets/js/*.js', ['js']);
-}));
+gulp.task('default', gulp.parallel(['html', 'css', 'copy']));
